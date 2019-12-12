ee48528cc35c7e17a8a33a7d8dca69c5f4e532f7
Bogdan Alin <ep.bogdy@gmail.com>
2019-12-13 00:22:25 +0200

    css plays

--- src/app/components/start/start.component.ts
+++ src/app/components/start/start.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-start',
@@ -7,7 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('first', {static: false}) one:ElementRef;
+  @ViewChild('second', {static: false}) two:ElementRef;
+  @ViewChild('third', {static: false}) three:ElementRef;
+  @ViewChild('fourth', {static: false}) four:ElementRef;
+  @ViewChild('fifth', {static: false}) five:ElementRef;
+
+  @HostListener('window:scroll', ['$event']) function() {
+      console.log(window.pageYOffset);
+      let scroller = window.pageYOffset
+
+        if(scroller >= 200 && scroller <= 400){
+          console.log(this.one);
+          this.rendered.setStyle(this.one.nativeElement,'transition','1s linear');
+          this.rendered.setStyle(this.one.nativeElement,'opacity','1');
+        }
+        if(scroller >= 400 && scroller <= 600){
+          console.log(this.one);
+          this.rendered.setStyle(this.two.nativeElement,'transition','1s linear');
+          this.rendered.setStyle(this.two.nativeElement,'opacity','1');
+        }
+        if(scroller >= 1000 && scroller <=1200){
+          console.log(this.one);
+          this.rendered.setStyle(this.three.nativeElement,'transition','1s linear');
+          this.rendered.setStyle(this.three.nativeElement,'opacity','1');
+        }
+        if(scroller >= 1500 && scroller <= 1700){
+          console.log(this.one);
+          this.rendered.setStyle(this.four.nativeElement,'transition','1s linear');
+          this.rendered.setStyle(this.four.nativeElement,'opacity','1');
+        }
+        if(scroller >= 2000){
+          console.log(this.one);
+          this.rendered.setStyle(this.five.nativeElement,'transition','1s linear');
+          this.rendered.setStyle(this.five.nativeElement,'opacity','1');
+        }
+
+  }  
+
+  constructor(private rendered: Renderer2) { }
 
   ngOnInit() {
   }
